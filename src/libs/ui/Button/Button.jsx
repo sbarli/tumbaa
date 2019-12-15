cb6772ee41b2db95eae44b5efa58e02d2011e0cc
Samantha Salley <samanthasalley@gmail.com>
2019-12-14 19:20:52 -0800

    (add)  🎨  sizes prop and styles (update)  🎨  themes

--- src/libs/ui/Button/Button.jsx
+++ src/libs/ui/Button/Button.jsx
@@ -4,28 +4,63 @@ import PropTypes from 'prop-types';
 // styled components
 import styled from 'styled-components/macro';
 
+const buttonSizes = {
+  sm: `
+    padding: 8px;
+    font-size: 1.8rem;
+  `,
+  md: `
+    padding: 10px;
+    font-size: 2rem;
+  `,
+  lg: `
+  padding: 15px;
+  font-size: 2.2rem;
+  `,
+};
+
 const themes = {
-  green: `
-    color: white;
-    background: green;
-    border: 1px solid darkgreen;
-    border-radius: var(--input-radius);
-    padding: 5px;
+  gold: `
+    color: var(--blurple-dark);
+    background-color: var(--gold-light);
+    border: 4px solid var(--gold-light);
+    border-radius: var(--radius-default);
+
+    &:hover {
+      background-color: var(--gold-primary);
+      border-color: var(--gold-primary);
+    }
+  `,
+  blurple: `
+    color: var(--blurple-light);
+    border: 4px solid var(--blurple-light);
+    border-radius: var(--radius-default);
+
+    &:hover {
+      color: var(--blurple-primary);
+      border-color: var(--blurple-primary);
+    }
   `,
   transparent: `
-    color: black;
     background: transparent;
     border: none;
     border-radius: none;
   `,
 };
 
-const ButtonWrapper = styled.button(({ theme }) => `
+const ButtonWrapper = styled.button(({ theme, size }) => `
+  font-weight: var(--font-weight-bold);
+  color: var(--charcoal-primary);
+  cursor: pointer;
+
+  ${buttonSizes[size]}
   ${themes[theme]}
-  width: 15rem;
-  height: 3.5rem;
-  font-weight: var(--bold-font-weight);
-  font-size: var(--normal-font-size);
+
+  &:disabled {
+    color: var(--charcoal-light);
+    border-color: var(--charcoal-light);
+    cursor: default;
+  }
 `);
 
 const Button = (props) => {
@@ -36,12 +71,16 @@ const Button = (props) => {
 
 Button.defaultProps = {
   type: 'button',
+  size: 'md',
 };
 
 Button.propTypes = {
   children: PropTypes.any.isRequired,
+  size: PropTypes.oneOf([
+    'sm', 'md', 'lg',
+  ]).isRequired,
   theme: PropTypes.oneOf([
-    'green', 'transparent'
+    'gold', 'transparent', 'blurple',
   ]).isRequired,
   type: PropTypes.oneOf([
     'button', 'submit'
