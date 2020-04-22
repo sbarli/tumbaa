90875a8fad5aaab85cabd1457f05c3a0fd99fb9f
Samantha Salley <samanthasalley@gmail.com>
2020-04-22 16:15:08 -0700

    (create)  ✨  new base Figure UI component

---
+++ src/libs/ui/Figure/Figure.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Link from '../Link/Link';
+
+/**
+ * ===========================
+ *     STYLED COMPONENTS
+ * ===========================
+ */
+import styled from 'styled-components/macro';
+
+/**
+ * ==================
+ *     COMPONENT
+ * ==================
+ */
+const Figure = ({ imgSrc, altText, caption, linkTo }) => {
+  const ImageAsset = require('../../../assets/images/' + imgSrc);
+  const ImageComponent = (
+    <figure>
+      <img src={ImageAsset} alt={altText}></img>
+      {caption ? <caption>{caption}</caption> : null}
+    </figure>
+  );
+  return linkTo
+    ? (
+      <Link linkTo={linkTo}>
+        {ImageComponent}
+      </Link>
+    )
+    : ImageComponent
+};
+
+/**
+ * ==================
+ *     PROP-TYPES
+ * ==================
+ */
+
+Figure.defaultProps = {
+  altText: '',
+};
+
+Figure.propTypes = {
+  imgSrc: PropTypes.string.isRequired,
+  altText: PropTypes.string,
+  caption: PropTypes.string,
+  linkTo: PropTypes.string,
+};
+
+export default Figure;
