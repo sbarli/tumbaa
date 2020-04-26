84eb9934da2c1921e4117a44aafbe109bf97ee74
Samantha Salley <samanthasalley@gmail.com>
2020-04-26 15:55:38 -0700

    (update)  🧠  to grab post assets and generate dynamic BlogPost components from the data

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import blogs from '../assets/blogs';
+import BlogPost from '../BlogPost/BlogPost';
+import Header from '../libs/ui/Header/Header';
+
 import styled from 'styled-components/macro';
 
 const Blog = () => {
+  const BlogPosts = Object.values(blogs)
+    .map(blog => (
+      <BlogPost {...blog} />
+    ));
   return (
-    <div>Blog Page</div>
+    <div>
+      <Header size="h1">Public Works</Header>
+      {BlogPosts}
+    </div>
   );
 };
 
