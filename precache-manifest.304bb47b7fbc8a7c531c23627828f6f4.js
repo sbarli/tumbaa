3b1b911689c59c5aa590b9046fe3195b2a2a233e
Samantha Salley <samanthasalley@gmail.com>
2020-04-26 16:41:51 -0700

    Updates

---
+++ precache-manifest.304bb47b7fbc8a7c531c23627828f6f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef41ba652e78490b065f7cd6fbbf3a78",
+    "url": "/tumbaa/index.html"
+  },
+  {
+    "revision": "544c0a8ce974052119c5",
+    "url": "/tumbaa/static/css/main.c1dd718c.chunk.css"
+  },
+  {
+    "revision": "91d681588ce05d5e1026",
+    "url": "/tumbaa/static/js/2.bd8c7dd3.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/tumbaa/static/js/2.bd8c7dd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "544c0a8ce974052119c5",
+    "url": "/tumbaa/static/js/main.32b717dd.chunk.js"
+  },
+  {
+    "revision": "3079f72b351583e2519a",
+    "url": "/tumbaa/static/js/runtime-main.a6b85f1d.js"
+  },
+  {
+    "revision": "5b761a008c03d7e7abf20dfff92dcd0f",
+    "url": "/tumbaa/static/media/headshot.5b761a00.jpg"
+  }
+]);
