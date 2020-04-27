b324814ec5048dee60e09fd5a6c10ee71f4f6753
Samantha Salley <samanthasalley@gmail.com>
2020-04-26 20:59:58 -0700

    (add)  ✏️  publishDate to display

--- src/components/BlogPost/BlogPost.jsx
+++ src/components/BlogPost/BlogPost.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import moment from 'moment';
 import PropTypes from 'prop-types';
 
 import Link from '../../libs/ui/Link/Link';
 import Group from '../../libs/ui/Group/Group';
 import Header from '../../libs/ui/Header/Header';
 import Section from '../../libs/ui/Section/Section';
+import Paragraph from '../../libs/ui/Paragraph/Paragraph';
 
 const BlogPost = ({
   id,
   title,
-  content,
   linkTo,
+  content,
+  publishDate,
 }) => (
   <Group>
-    <Section key={id}>
-      <Header size="h2" removeMarginTop>{title}</Header>
+    <Section>
+      <Header size="h2" removeMarginTop removeMarginBottom>{title}</Header>
+      <Paragraph content={`<strong>${moment(publishDate).format('MMMM DD YYYY')}</strong>`} isHTML />
       <div dangerouslySetInnerHTML={{ __html: content }} />
       {linkTo && <Link linkTo={linkTo}>Read More</Link>}
     </Section>
